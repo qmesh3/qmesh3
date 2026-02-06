bb77cbb9435cdb3e5351c50d12accdcc658a2cd8
Jon Hill <jon.hill@york.ac.uk>
2026-02-06 11:33:36 +0000

    Moving to modern installs

--- qmesh3/__init__.py
+++ qmesh3/__init__.py
@@ -25,66 +25,93 @@ import qmesh3.lib
 from qmesh3.config import *
 
 import pkg_resources
-#Set the version attribute
-#try:
-#    __packaged_distro__ = pkg_resources.get_distribution('qmesh3')
-#    __version__ = __packaged_distro__.version
-#except (pkg_resources.DistributionNotFound, AttributeError, NameError):
-#    __version__ = "local"
-#    LOG.warning('Could not find qmesh version information. Provenance information is incomplete.')
-##Set the git-sha-key attribute
-#try:
-#    __packaged_distro__.has_metadata('GIT_SHA_KEY')
-#    __git_sha_key__ = __packaged_distro__.get_metadata('GIT_SHA_KEY')
-#except (AttributeError, FileNotFoundError, NameError):
-#    __git_sha_key__ = "local"
-#    LOG.warning('Could not find qmesh origin git SHA key. Provenance information is incomplete.')
-##Set the license attribute
-#try:
-#    __packaged_distro__.has_metadata('LICENSE')
-#    __license__ = __packaged_distro__.get_metadata('LICENSE')
-#except (AttributeError, FileNotFoundError, NameError):
-#    __license__ = ""
-#    LOG.warning('Could not find complete license statement. Provenance information is incomplete.')
-#    LOG.warning('Qmesh is distributed under GPLv3. Please observe the license at .')
-##Set the authors attribute
-#try:
-#    __packaged_distro__.has_metadata('AUTHORS.md')
-#    __authors__ = __packaged_distro__.get_metadata('AUTHORS.md')
-#except (AttributeError, FileNotFoundError, NameError):
-#    __authors__ = ""
-#    LOG.warning('Could not find qmesh authors information. Provenance information is incomplete.')
-## Set the gmsh-path attribute
-#try:
-#    __packaged_distro__.has_metadata('GMSH_BIN_PATH')
-#    __gmsh_bin_path__ = __packaged_distro__.get_metadata('GMSH_BIN_PATH')
-#except (AttributeError, FileNotFoundError, NameError):
-#    __gmsh_bin_path__ = "/usr/bin"
-#    LOG.warning('Could not find gmsh binary path specification. Mesh generation might fail.')
-## Set the qgis-install-path attribute
-#try:
-#    __packaged_distro__.has_metadata('QGIS_PATH')
-#    __qgis_path__ = __packaged_distro__.get_metadata('QGIS_PATH')
-#except (AttributeError, FileNotFoundError, NameError):
-#    __qgis_path__ = "/usr/local/"
-#    LOG.warning('Could not find qgis path specification. qgis initialisation might fail.')
-#try:
-#    del __packaged_distro__
-#except NameError:
-#    pass
-__version__ = "local"
-__git_sha_key__ = "local"
-__license__ = ""
-__authors__ = ""
-__gmsh_bin_path__ = "/usr/bin"
-__qgis_path__ = "/usr/local/"
-
-
-# Set-up environment and initialise qgis, without graphics.
 # Suppress verbose debugging Qt messages
-os.environ['QT_LOGGING_RULES']="qt5ct.debug=false"
+os.environ['QT_LOGGING_RULES'] = "qt5ct.debug=false"
 # Make sure qmesh can run in environments without graphics
 os.environ["QT_QPA_PLATFORM"] = "offscreen"
+
+import sys
+import shutil
+import importlib
+import platform
+
+def _check_env():
+    # Check for QGIS
+    try:
+        importlib.import_module('qgis')
+    except ModuleNotFoundError:
+        print("Error: Host system does not appear to have a qgis installation.")
+        print("Qmesh uses qgis for GIS operations.")
+        # In runtime, we usually exit or warn rather than raising Distutils errors
+        sys.exit(1)
+
+    # Check for GMSH
+    gmsh_path = shutil.which("gmsh")
+    if not gmsh_path and platform.system() == 'Windows':
+        # Fallback check for Windows if not in PATH
+        gmsh_path = shutil.which("gmsh.exe")
+
+    if not gmsh_path:
+        print("Error: Could not locate a gmsh installation.")
+        print("Qmesh uses gmsh as a mesh generator.")
+        sys.exit(1)
+
+_check_env()
+
+#Set the version attribute
+try:
+    __packaged_distro__ = pkg_resources.get_distribution('qmesh3')
+    __version__ = __packaged_distro__.version
+except (pkg_resources.DistributionNotFound, AttributeError, NameError):
+    __version__ = "local"
+    LOG.warning('Could not find qmesh version information. Provenance information is incomplete.')
+#Set the git-sha-key attribute
+try:
+    __packaged_distro__.has_metadata('GIT_SHA_KEY')
+    __git_sha_key__ = __packaged_distro__.get_metadata('GIT_SHA_KEY')
+except (AttributeError, FileNotFoundError, NameError):
+    __git_sha_key__ = "local"
+    LOG.warning('Could not find qmesh origin git SHA key. Provenance information is incomplete.')
+#Set the license attribute
+try:
+    __packaged_distro__.has_metadata('LICENSE')
+    __license__ = __packaged_distro__.get_metadata('LICENSE')
+except (AttributeError, FileNotFoundError, NameError):
+    __license__ = ""
+    LOG.warning('Could not find complete license statement. Provenance information is incomplete.')
+    LOG.warning('Qmesh is distributed under GPLv3. Please observe the license at .')
+#Set the authors attribute
+try:
+    __packaged_distro__.has_metadata('AUTHORS.md')
+    __authors__ = __packaged_distro__.get_metadata('AUTHORS.md')
+except (AttributeError, FileNotFoundError, NameError):
+    __authors__ = ""
+    LOG.warning('Could not find qmesh authors information. Provenance information is incomplete.')
+# Set the gmsh-path attribute
+try:
+    __packaged_distro__.has_metadata('GMSH_BIN_PATH')
+    __gmsh_bin_path__ = __packaged_distro__.get_metadata('GMSH_BIN_PATH')
+except (AttributeError, FileNotFoundError, NameError):
+    __gmsh_bin_path__ = "/usr/bin"
+    LOG.warning('Could not find gmsh binary path specification. Mesh generation might fail.')
+# Set the qgis-install-path attribute
+try:
+    __packaged_distro__.has_metadata('QGIS_PATH')
+    __qgis_path__ = __packaged_distro__.get_metadata('QGIS_PATH')
+except (AttributeError, FileNotFoundError, NameError):
+    __qgis_path__ = "/usr/local/"
+    LOG.warning('Could not find qgis path specification. qgis initialisation might fail.')
+try:
+    del __packaged_distro__
+except NameError:
+    pass
+#__version__ = "local"
+#__git_sha_key__ = "local"
+#__license__ = ""
+#__authors__ = ""
+#__gmsh_bin_path__ = "/usr/bin"
+#__qgis_path__ = "/usr/local/"
+
 # Initialise qgis
 qgs = QgsApplication([], True, None)
 qgis_install_path='/usr'
